e2f4f53475c2a4a853be61bff5e4937521e2c734
leyla li <leylali@qq.com>
2019-10-07 09:00:17 -0700

    done with documentation

--- src/model/mongoose-model.js
+++ src/model/mongoose-model.js
@@ -1,14 +1,30 @@
 'use strict';
 
 class DataModel {
+  /**
+   * Model Constructor
+   * @param schema {object} - mongo schema
+   */
   constructor(schema){
     this.schema = schema;
   }
+
+  /**
+   * Get data with or without id
+   * @param _id {string}
+   * @returns {object}
+   */
 
   get(id) {
     let query = id ? { _id: id } : {};  
     return this.schema.find(query);
   }
+
+  /**
+   * Create a new record
+   * @param record {object} matches the format of the schema
+   * @returns {*}
+   */
 
   post(record) {
     console.log(record);
@@ -16,12 +32,26 @@ class DataModel {
     return newRecord.save();
   }
 
+  
+  /**
+   * Update a data that has the given id with the given record in the database
+   * @param _id {string} Mongo Record ID
+   * @param record {object} The record data to replace. ID is a required field
+   * @returns {*}
+   */
+
   put(id,record) {
     const filter = { _id: id };
     const update = record;
 
     return this.schema.findOneAndUpdate(filter, update);
   }
+
+  /**
+   * Deletes a recod in the model
+   * @param _id {string} Mongo Record ID
+   * @returns {*}
+   */
 
   delete(id) {
     const filter = { _id: id };
--- src/route/api.js
+++ src/route/api.js
@@ -6,18 +6,59 @@ const router = express.Router();
 //use model-finder middleware to import models
 const modelFinder = require('../middleware/model-finder');
 
-// const auth = require('../middleware/auth');
-//define a parameter which would run our middleware, it is always the first mw to run
 router.param('model', modelFinder);
 
 //define a consistent set of routes that can be reusable for all the modles
+
+/**
+ * @route GET /api/v1/:model
+ * @returns {string} 200 a status code means okay
+ * @returns {object}  return all the data
+ * @returns {Error} returns an error
+ */
 router.get('/api/v1/:model', handleGetAll);
+
+/**
+ * @route GET /api/v1/:model/:id
+ * @returns {string} 200 a status code means okay
+ * @returns {object}  return all the data
+ * @returns {Error} returns an error
+ */
 router.get('/api/v1/:model/:id', handleGetOne);
 
 //if we add the auth() as middleware, we have to add headers (username, password) to make the route work
+
+/**
+ * @route POST /api/v1/:model
+ * @returns {string} 200 a status code means okay
+ * @returns {object}  return all the data
+ * @returns {Error} returns an error
+ */
 router.post('/api/v1/:model', handlePost);
+
+/**
+ * @route PUT /api/v1/:model/:id
+ * @returns {string} 200 a status code means okay
+ * @returns {object}  return all the data
+ * @returns {Error} returns an error
+ */
 router.put('/api/v1/:model/:id', handlePut);
+
+/**
+ * @route DELETE /api/v1/:model/:id
+ * @returns {string} 200 a status code means okay
+ * @returns {object}  return all the data
+ * @returns {Error} returns an error
+ */
 router.delete('/api/v1/:model/:id', handleDelete);
+
+/**
+   * handler function for the route to get data from database
+   * @param request {object}
+   * @param response {object}
+   * @param next {function} a middleware function for the route to get data from the database
+   * @returns {string} the status code 200 and {object}
+   */
 
 function handleGetAll(req, res, next) {
   req.model.get()
@@ -27,6 +68,15 @@ function handleGetAll(req, res, next) {
     .catch(next);
 }
 
+/**
+   * handler function for the route to get one data with the given id from database
+   * @param request {object}
+   * @param response {object}
+   * @param request.params.id {string}
+   * @param next {function} a middleware function for the route to get data with the id from the database
+   * @returns {string} the status code 200 and {object}
+   */
+
 function handleGetOne(req, res, next) {
   req.model.get(req.params.id)
     .then(result=>{
@@ -34,6 +84,15 @@ function handleGetOne(req, res, next) {
     })
     .catch(next);
 }
+
+/**
+   * handler function for the route to create data from database with the request.body
+   * @param request {object}
+   * @param response {object}
+   * @param request.body {object}
+   * @param next {function} a middleware function for the route to create data from the database
+   * @returns {string} the status code 201 and {object}
+   */
 
 function handlePost(req, res, next) {
   req.model.post(req.body)
@@ -43,6 +102,16 @@ function handlePost(req, res, next) {
     .catch(next);
 }
 
+/**
+   * handler function for the route to update one data with the given id from database with the reaquest body
+   * @param request {object}
+   * @param response {object}
+   * @param request.params.id {string}
+   * @param request.body {object}
+   * @param next {function} a middleware function for the route to delete data from the database
+   * @returns {string} the status code 200 and {object}
+   */
+
 function handlePut(req, res, next) {
   console.log('here', req.body);
   req.model.put(req.params.id, req.body)
@@ -51,6 +120,16 @@ function handlePut(req, res, next) {
     })
     .catch(next);
 }
+
+/**
+   * handler function for the route to delete one data with the given id from database
+   * @param request {object}
+   * @param response {object}
+   * @param request.params.id {string}
+   * @param request.body {object}
+   * @param next {function} a middleware function for the route to delete data from the database
+   * @returns {string} the status code 200 and {object}
+   */
 
 function handleDelete(req, res, next) {
   req.model.delete(req.params.id)
